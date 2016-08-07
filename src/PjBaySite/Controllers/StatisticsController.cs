aaacc705faab7afe9bfed9557a251380d13701d6
liron <liron@DESKTOP-DKDL8L9>
2016-08-07 20:30:38 +0300

    started the stats

---
+++ src/PjBaySite/Controllers/StatisticsController.cs
@@ -0,0 +1,30 @@
+using System.Linq;
+using Microsoft.AspNet.Mvc;
+using PjBaySite.Models;
+
+namespace PjBaySite.Controllers
+{
+    public class StatisticsController : Controller
+    {
+        private ApplicationDbContext _context;
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GraphA()
+        {
+            /*
+            var queryField = from fields in _context.Fields
+                             join c in _context.Courses on fields.ID equals c.Field.ID
+                             join i in _context.Institutes on c.InstatuteID equals i.ID
+                             join p in _context.Projects on c.ID equals p.CourseID
+                             where i.Name.Equals(Name) && p.Purchased == false
+                             select fields.fieldName;
+
+            SelectList ListFields = new SelectList(queryField.Distinct());
+            return Json(ListFields);
+            */
+        }
+    }
+}
